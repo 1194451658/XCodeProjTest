d076ea88225ead7f3e21d6f8e4a1b49a794b503b
wanzhang <1194451658@qq.com>
2019-01-25 23:02:46 +0800

    Initial commit

---
+++ Assets/TestPersistentDataPath/TestPersistentDataPath.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPersistentDataPath : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		Debug.Log ("======== 测试写入PersistentDataPath =========");
+		Debug.LogErrorFormat ("PersistentDataPath:{0}", Application.persistentDataPath);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
